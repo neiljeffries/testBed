afdad74a2a8854c1a794f4362c1e2daba80ffa76
Jeffries Neil (sdf1ndj) <SDF1NDJ@UPS.COM>
2018-05-17 16:24:02 -0400

    nab bar upds

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
-import { NgModule }             from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FrogDashboardComponent } from "app/frog-dashboard/frog-dashboard.component";
-import { CrewmembersComponent } from "app/crewmembers/crewmembers.component";
-import { ProfileComponent } from "app/profile/profile.component";
-import { DataTableComponent } from "app/data-table/data-table.component";
-import { HubxDashboardComponent } from "app/hubx-dashboard/hubx-dashboard.component";
-import { FlightsTableComponent } from "app/flights-table/flights-table.component";
+import { FrogDashboardComponent } from 'app/frog-dashboard/frog-dashboard.component';
+import { CrewmembersComponent } from 'app/crewmembers/crewmembers.component';
+import { ProfileComponent } from 'app/profile/profile.component';
+import { DataTableComponent } from 'app/data-table/data-table.component';
+import { HubxDashboardComponent } from 'app/hubx-dashboard/hubx-dashboard.component';
+import { FlightsTableComponent } from 'app/flights-table/flights-table.component';
 
 const routes: Routes = [
   { path: 'frogs', component: FrogDashboardComponent },
-  { path: 'crew', component: CrewmembersComponent },  
-  { path: '', redirectTo: '/crew', pathMatch: 'full' },
+  { path: 'crew', component: CrewmembersComponent },
+  { path: '', redirectTo: '/flights', pathMatch: 'full' },
   { path: 'profile', component: ProfileComponent },
   { path: 'datatable', component: DataTableComponent },
   { path: 'hubx', component: HubxDashboardComponent },
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -12,4 +12,9 @@ export class NavbarComponent implements OnInit {
   ngOnInit() {
   }
 
+
+  clickedSlideMenu(event) {
+    // emit event
+
+  }
 }
