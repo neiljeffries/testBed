2477d1a0acf6f7ed5f9b33339ab881aeb483014b
Jeffries Neil (sdf1ndj) <SDF1NDJ@UPS.COM>
2018-05-29 17:10:23 -0400

    added LTP load classes

---
+++ src/app/classes/fids-load-position.ts
@@ -0,0 +1,7 @@
+export class FidsFlightLoadPosition {
+    positionLabel: string; // 'AB' or '2L' etc...
+    unitLoadingDevice: string; // uld number or 'BELLY' or '*** VOID ***'
+    weight: string; // weight in lbs
+    positionLock: string; // Y or N
+    positionStatus: string; // 'FILLED' or 'VOID'
+};
