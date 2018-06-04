928418fe457ba93bd840bf751018a4eb4d17dcf4
Jeffries Neil (sdf1ndj) <SDF1NDJ@UPS.COM>
2018-06-04 18:52:15 -0400

    upd

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { FidsFlight } from '../classes/fids-flight';
-// import { A300600Component } from '../airplanes/a300600/a300600.component';
 
 @Component({
   selector: 'app-dialog',
@@ -15,9 +14,8 @@ export class DialogComponent implements OnInit {
   flightDetail: FidsFlight = null;
   showPositionsLoaded = true;
   removePositionsLoaded = false;
-  // displayedColumns = ['Flight Date', 'Sched Time', 'Est Time', 'Est Load Time', 'Last ULD Time', 'State', 'Status'];
-  detailDisplayCols = ['flightdate', 'schedtime', 'esttime', 'estloadtime', 'lastuldloadtime', 'state'];
-  dataSource = [];
+ // detailDisplayCols = ['flightdate', 'schedtime', 'esttime', 'estloadtime', 'lastuldloadtime', 'state'];
+ // dataSource = [];
 
 
   constructor(
@@ -35,17 +33,17 @@ export class DialogComponent implements OnInit {
 
   ngOnInit() {
 
-    const DETAIL_DATA = [{
-      flightdate: this.data.flightDetail[0].flightDate.millis,
-      schedtime: this.data.flightDetail[0].flightSchedTime,
-      esttime: this.data.flightDetail[0].flightEstTime,
-      estTimeDiff: this.data.flightDetail[0].flightEstTimeDiff,
-      estloadtime: this.data.flightDetail[0].flightLoadEstimate,
-      lastuldloadtime: this.data.flightDetail[0].flightLastUldTs,
-      state: this.data.flightDetail[0].flightState
-    }];
+    // const DETAIL_DATA = [{
+    //   flightdate: this.data.flightDetail[0].flightDate.millis,
+    //   schedtime: this.data.flightDetail[0].flightSchedTime,
+    //   esttime: this.data.flightDetail[0].flightEstTime,
+    //   estTimeDiff: this.data.flightDetail[0].flightEstTimeDiff,
+    //   estloadtime: this.data.flightDetail[0].flightLoadEstimate,
+    //   lastuldloadtime: this.data.flightDetail[0].flightLastUldTs,
+    //   state: this.data.flightDetail[0].flightState
+    // }];
 
-    this.dataSource = DETAIL_DATA
+    // this.dataSource = DETAIL_DATA
 
     this.dialogRef.afterOpen().subscribe(() => {
       console.log('after open fired');
