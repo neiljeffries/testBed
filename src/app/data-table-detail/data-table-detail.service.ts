05f0e3586746b0d0bc051a5c5ad3fc9e2508744d
neiljeffries <neiljeffries@gmail.com>
2018-06-09 09:49:52 -0400

    upds

--- src/app/data-table-detail/data-table-detail.service.ts
+++ src/app/data-table-detail/data-table-detail.service.ts
@@ -1,12 +1,13 @@
+
+import {throwError as observableThrowError,  Observable } from 'rxjs';
 /*
 Remember to add DataTableDetailService to providers in your app.module!
 */
 import { Http, Response } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch'
-import { Person } from './person';
+
+
+
 
 @Injectable()
 export class DataTableDetailService {
@@ -14,11 +15,11 @@ export class DataTableDetailService {
   private url = 'ssets/data/100k.json';
   constructor(private http: Http) { }
 
-  getData(): Observable<Person[]> {
-    return this.http.get(this.url)
-      .map(this.extractData)
-      .catch(this.handleErrorObservable);
-  }
+  // getData(): Observable<Person[]> {
+  //   return this.http.get(this.url)
+  //     .map(this.extractData)
+  //     .catch(this.handleErrorObservable);
+  // }
 
   private extractData(res: Response) {
     const body = res.json();
@@ -27,6 +28,6 @@ export class DataTableDetailService {
 
   private handleErrorObservable(error: Response | any) {
     console.error(error.message || error);
-    return Observable.throw(error.message || error);
+    return observableThrowError(error.message || error);
   }
 }
